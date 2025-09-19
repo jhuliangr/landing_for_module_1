d05e87187d87ec9f2644dbdcc7b57b50e9159a8e
Sito8943 <sito8943@gmail.com>
2025-09-19 11:03:44 +0200

    section 1 styles and responsive

--- src/components/WavesGroup.tsx
+++ src/components/WavesGroup.tsx
@@ -9,8 +9,8 @@ function WavesGroup({
     <div
       className={`${position} flex gap-5 text-[#C1C9D4]`}
     >
-      <Waves className="w-20 md:w-fit md:block" />
-      <Waves className="w-20 md:w-fit md:block" />
+      <Waves className="lg:w-50 w-20 md:block" />
+      <Waves className="lg:w-50 w-20 md:block" />
     </div>
   );
 }
--- src/components/sections/Section1.tsx
+++ src/components/sections/Section1.tsx
@@ -3,49 +3,38 @@ import { WavesGroup } from "../WavesGroup";
 
 function Section1() {
   return (
-    <div className="md:h-[1080px] grid md:grid-cols-2">
-      <div className="relative pt-56 pb-20 md:pt-0 md:h-full">
-        <div className="h-full flex justify-center items-center max-w-[100vw]">
-          <div className="flex flex-col justify-center items-center w-[80%] md:w-[654px] gap-10">
-            <p className="text-primary text-4xl md:text-[64px] font-bold">
-              Shocking and Emotional
-            </p>
-            <p className="font-normal text-sm md:text-[32px] text-black">
-              With SafeBox, the smart solar-powered sand lockers, you can store
-              your valuables and enjoy the beach without worries.
-            </p>
-            <div className="flex gap-10 justify-start w-full">
-              <Button type="button" text="Book your locker" />
-              <Button
-                type="button"
-                variant="secondary"
-                text="Find out how it works"
-              />
-            </div>
-          </div>
+    <div className="md:min-h-screen py-26 pt-56 px-3 lg:px-30 md:px-10 gap-30 grid lg:grid-cols-2 relative">
+      <WavesGroup position="hidden md:flex absolute lg:bottom-10 -bottom-10 left-5" />
+      <div className="flex flex-col justify-center items-center md:items-start gap-10">
+        <h2 className="text-primary max-lg:text-center text-4xl md:text-6xl font-bold">
+          Shocking and Emotional
+        </h2>
+        <p className="font-normal text-base text-black text-center md:w-3/4 md:text-left">
+          With SafeBox, the smart solar-powered sand lockers, you can store your
+          valuables and enjoy the beach without worries.
+        </p>
+        <div className="flex max-sm:flex-col gap-4 justify-start w-full">
+          <Button type="button" text="Book your locker" />
+          <Button
+            type="button"
+            variant="secondary"
+            text="Find out how it works"
+          />
         </div>
-        <WavesGroup position="hidden md:flex absolute bottom-10 left-5" />
       </div>
-      <div className="hidden md:grid grid-cols-3 md:grid-cols-8 gap-5 px-5 md:px-0 pb-20">
-        <div className="md:col-span-3 flex items-center">
-          <img
-            src="/assets/pictures/1.png"
-            className="h-48 md:h-[699px] object-cover rounded-[100px]"
-          />
-        </div>
-        <div className="md:col-span-2 flex items-center">
-          <img
-            src="/assets/pictures/2.png"
-            className="h-48 md:h-[699px] object-cover rounded-[100px]"
-          />
-        </div>
-        <div className="flex items-center">
-          <img
-            src="/assets/pictures/1.png"
-            className="h-48 md:h-[699px] object-cover rounded-[100px]"
-          />
-        </div>
-        <div className="hidden md:block col-span-2"></div>
+      <div className="hidden lg:grid grid-cols-3 md:grid-cols-8 gap-5 px-5 md:px-0 pb-20">
+        <img
+          src="/assets/pictures/1.png"
+          className="md:h-[699px] object-cover rounded-[100px] col-span-4"
+        />
+        <img
+          src="/assets/pictures/2.png"
+          className="md:h-[699px] object-cover rounded-[100px] col-span-3"
+        />
+        <img
+          src="/assets/pictures/1.png"
+          className="md:h-[699px] object-cover rounded-[100px] col-span-1"
+        />
       </div>
     </div>
   );
