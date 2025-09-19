2e0fc200920bc299aa233f173d701365689d87ac
Sito8943 <sito8943@gmail.com>
2025-09-19 11:13:31 +0200

    using bars as svg and fixed styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import { SITEMAP } from "../constants";
+import Bars from "./svg/Bars";
 import Logo from "./svg/Logo";
 
 function Header() {
   return (
-    <div className="max-w-full w-full bg-primary h-30 px-4 md:px-28 py-3 flex items-center justify-between fixed z-20">
+    <header className="max-w-full w-full bg-primary h-30 px-4 md:px-28 py-3 flex items-center justify-between fixed z-20">
       <a href="#" className="flex items-center gap-5">
-        <Logo width={64} height={64} />
-        <h1 className="text-secondary font-bold text-5xl pt-2">SafeBox</h1>
+        <Logo className="w-16 h-16" />
+        <h1 className="text-secondary font-bold text-3xl sm:text-5xl pt-2">SafeBox</h1>
       </a>
       <nav className="max-md:hidden">
         <ul className="flex justify-between gap-10">
@@ -14,7 +15,7 @@ function Header() {
             <li key={link.url}>
               <a
                 href={`${link.url}`}
-                className="text-white hover:text-secondary font-normal text-3xl"
+                className="text-white hover:text-secondary font-normal text-base"
               >
                 {link.text}
               </a>
@@ -23,13 +24,9 @@ function Header() {
         </ul>
       </nav>
       <div className="md:hidden flex flex-col gap-1">
-        <div className="w-6 h-1 bg-white" />
-
-        <div className="w-6 h-1 bg-white" />
-
-        <div className="w-6 h-1 bg-white" />
+        <Bars />
       </div>
-    </div>
+    </header>
   );
 }
 
